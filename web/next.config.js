e563c27f123b6af1543f9195345e97371495b4b8
streetunity <48616056+streetunity@users.noreply.github.com>
2025-09-21 17:29:37 -0500

    Update next.config.js for AWS deployment

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  },
+  publicRuntimeConfig: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'
+  }
 };
 module.exports = nextConfig;
