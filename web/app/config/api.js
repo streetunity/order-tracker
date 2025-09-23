c7a19a405dabec1c4148751653800a38cbd38840
streetunity <48616056+streetunity@users.noreply.github.com>
2025-09-23 10:44:43 -0500

    Create hardcoded API config file

---
+++ web/app/config/api.js
@@ -0,0 +1,6 @@
+// HARDCODED API CONFIGURATION FOR AWS DEPLOYMENT
+// Change this when moving servers
+export const API_BASE = 'http://50.19.66.100:4000';
+
+// This bypasses all environment variable issues
+// To change the API server, only update this file
